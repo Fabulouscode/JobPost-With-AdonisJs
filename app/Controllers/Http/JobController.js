38eadc520f70d8c11d619ed7bacadad0d7f05589
Fabulouscode <ulaghaondo@gmail.com>
2022-03-19 09:30:21 +0100

    refactor

--- app/Controllers/Http/JobController.js
+++ app/Controllers/Http/JobController.js
@@ -2,6 +2,13 @@
 
 const Job = use('App/Models/Job');
 class JobController {
+
+    async home({view}) {
+        // Fetch a job
+        const jobs = await Job.all();
+
+        return view.render('index', { jobs: jobs.toJSON() })
+    }
     async userIndex({view, auth}) {
 
         // Fetch all user's jobs
